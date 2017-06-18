f617cdfed884792c7ffed523582f6cd487b0104e
Daniel Elizondo Sanchez <daniel.elizondo@ucrso.info>
2017-06-18 21:39:38 +0000

    Quitar user Feedback de Sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,10 +50,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-         return response()->view('errors.500', [
-            'sentryID' => $this->sentryID,
-        ], 500);
-        //return parent::render($request, $exception);
+         //return response()->view('errors.500', [
+         //   'sentryID' => $this->sentryID,
+        //], 500);
+        return parent::render($request, $exception);
     }
     
 
