10e55897019c47f0fa4906aa3d0f0e67c78cd7e7
Daniel Elizondo Sanchez <daniel.elizondo@ucrso.info>
2017-07-21 23:36:15 +0000

    Se coloca sombra al título de Cuenta Nueva

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 <!-- Section Title -->
     <section id="section-title" class="bg-alternative">
         <div id="top-img-bg">
-            <h1 class="text-center">Cuenta Nueva</h1>
+            <h1 class="text-center sombraLetrasEncabezado">Cuenta Nueva</h1>
         </div>
     </section>
     <!-- End Section Title -->
